a271fc3d50281392fea43b4aac9a025c6ddea98b
Mateusz Fesz <mateuszfesz@wp.pl>
2025-02-06 21:22:05 +0100

    separate input from move

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,6 @@ struct WindowData<'a> {
     thread: &'a mut RaylibThread,
     frames_counter: usize,
 
-    allow_move: bool,
     offset: Vector2,
     pause: bool,
 }
@@ -55,11 +54,11 @@ struct GameState<'a> {
     // Representation of game (environment)
     // Shouldn't rely on Raylib
     game_over: bool,
+    allow_move: bool,
 
     fruit_position: Option<(isize, isize)>,
     snake_position: Vec<(isize, isize)>,
     snake_velocity: (isize, isize),
-    counter_tail: isize,
     board_size: (isize, isize),
 }
 
@@ -70,7 +69,6 @@ impl<'a> GameState<'a> {
                 handle: h,
                 thread: t,
                 frames_counter: 0,
-                allow_move: false,
                 offset: Vector2 { x: 0.0, y: 0.0 },
                 pause: false,
             })
@@ -83,8 +81,7 @@ impl<'a> GameState<'a> {
             control_mode: Mode::Keyboard,
 
             game_over: false,
-
-            counter_tail: 1,
+            allow_move: false,
 
             snake_position: vec![(0, 0)],
             fruit_position: None,
@@ -94,101 +91,113 @@ impl<'a> GameState<'a> {
     }
     fn reset(&mut self) {
         self.game_over = false;
-        self.counter_tail = 1;
         self.snake_position = vec![(0, 0)];
         self.fruit_position = None;
         self.snake_velocity = (1, 0);
     }
+
+    fn update_snake(&mut self) -> (isize, isize) {
+        let last_position = self.snake_position.last().unwrap().clone();
+        let saved_position = self.snake_position[0];
+        for i in (1..self.snake_position.len()).rev() {
+            self.snake_position[i] = self.snake_position[i - 1];
+        }
+        self.snake_position[0].0 = saved_position.0 + self.snake_velocity.0;
+        self.snake_position[0].1 = saved_position.1 + self.snake_velocity.1;
+
+        if self.snake_position[0].0 >= self.board_size.0
+            || self.snake_position[0].1 >= self.board_size.1
+            || self.snake_position[0].0 < 0
+            || self.snake_position[0].1 < 0
+        {
+            self.game_over = true;
+        }
+
+        for i in 1..self.snake_position.len() {
+            if self.snake_position[0] == self.snake_position[i] {
+                self.game_over = true;
+            }
+        }
+
+        self.allow_move = false;
+        last_position
+    }
+
+    fn update_env(&mut self, last_position: (isize, isize)) {
+        if let None = self.fruit_position {
+            let mut rng = rand::rng();
+            let random_position = (
+                rng.random_range(0..self.board_size.0 as i64) as isize,
+                rng.random_range(0..self.board_size.1 as i64) as isize,
+            );
+
+            if self.snake_position.iter().any(|p| *p == random_position) {
+                self.fruit_position = Some(last_position);
+            } else {
+                self.fruit_position = Some(random_position);
+            }
+        };
+
+        if let Some(pos) = self.fruit_position {
+            if self.snake_position[0] == pos {
+                self.snake_position.push(last_position);
+                self.fruit_position = None;
+            }
+        }
+    }
+
     fn update_game(&mut self) {
         match self.control_mode {
             Mode::Keyboard => {
                 if let Some(window) = &mut self.window {
                     if !self.game_over {
                         if !window.pause {
-                            if window.allow_move {
-                                match window.handle.get_key_pressed() {
-                                    Some(KeyboardKey::KEY_RIGHT) => {
-                                        if self.snake_velocity.0 == 0 {
-                                            self.snake_velocity = (1, 0);
-                                        }
-                                        window.allow_move = false;
+                            match window.handle.get_key_pressed() {
+                                Some(KeyboardKey::KEY_RIGHT) => {
+                                    if self.snake_velocity.0 == 0 {
+                                        self.snake_velocity = (1, 0);
+                                        self.allow_move = false;
                                     }
-                                    Some(KeyboardKey::KEY_LEFT) => {
-                                        if self.snake_velocity.0 == 0 {
-                                            self.snake_velocity = (-1, 0);
-                                        }
-                                        window.allow_move = false;
-                                    }
-                                    Some(KeyboardKey::KEY_DOWN) => {
-                                        if self.snake_velocity.1 == 0 {
-                                            self.snake_velocity = (0, 1);
-                                        }
-                                        window.allow_move = false;
-                                    }
-                                    Some(KeyboardKey::KEY_UP) => {
-                                        if self.snake_velocity.1 == 0 {
-                                            self.snake_velocity = (0, -1);
-                                        }
-                                        window.allow_move = false;
-                                    }
-                                    _ => (),
                                 }
+                                Some(KeyboardKey::KEY_LEFT) => {
+                                    if self.snake_velocity.0 == 0 {
+                                        self.snake_velocity = (-1, 0);
+                                        self.allow_move = false;
+                                    }
+                                }
+                                Some(KeyboardKey::KEY_DOWN) => {
+                                    if self.snake_velocity.1 == 0 {
+                                        self.snake_velocity = (0, 1);
+                                        self.allow_move = false;
+                                    }
+                                }
+                                Some(KeyboardKey::KEY_UP) => {
+                                    if self.snake_velocity.1 == 0 {
+                                        self.snake_velocity = (0, -1);
+                                        self.allow_move = false;
+                                    }
+                                }
+                                _ => (),
                             }
-
-                            let last_position = self.snake_position.last().unwrap().clone();
                             if window.frames_counter % 10 == 0 {
-                                let saved_position = self.snake_position[0];
-                                for i in (1..self.snake_position.len()).rev() {
-                                    self.snake_position[i] = self.snake_position[i - 1];
-                                }
-                                self.snake_position[0].0 = saved_position.0 + self.snake_velocity.0;
-                                self.snake_position[0].1 = saved_position.1 + self.snake_velocity.1;
-
-                                window.allow_move = true;
+                                self.allow_move = true;
                             }
                             window.frames_counter += 1;
-
-                            if self.snake_position[0].0 >= self.board_size.0
-                                || self.snake_position[0].1 >= self.board_size.1
-                                || self.snake_position[0].0 < 0
-                                || self.snake_position[0].1 < 0
-                            {
-                                self.game_over = true;
-                            }
-
-                            for i in 1..self.snake_position.len() {
-                                if self.snake_position[0] == self.snake_position[i] {
-                                    self.game_over = true;
-                                }
-                            }
-
-                            if let None = self.fruit_position {
-                                let mut rng = rand::rng();
-                                let random_position = (
-                                    rng.random_range(0..self.board_size.0 as i64) as isize,
-                                    rng.random_range(0..self.board_size.1 as i64) as isize,
-                                );
-
-                                if self.snake_position.iter().any(|p| *p == random_position) {
-                                    self.fruit_position = Some(last_position);
-                                } else {
-                                    self.fruit_position = Some(random_position);
-                                }
-                            };
-
-                            if let Some(pos) = self.fruit_position {
-                                if self.snake_position[0] == pos {
-                                    self.snake_position.push(last_position);
-                                    self.fruit_position = None;
-                                }
-                            }
                         }
                     } else if window.handle.is_key_pressed(KeyboardKey::KEY_ENTER) {
                         self.reset();
                     }
+                } else {
+                    panic!("Must use windowed mode for Keyboard control mode");
                 }
             }
             _ => (),
+        }
+        if self.allow_move {
+            let last_valid_position = self.update_snake();
+            self.update_env(last_valid_position);
+        } else {
+            self.update_env((0, 0));
         }
     }
     fn draw_game(&mut self) {
